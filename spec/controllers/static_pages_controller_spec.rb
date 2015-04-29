2dfa0672d07b1833b2d51cedd8928ff8d410aa35
Junichi Ito <jit@sonicgarden.jp>
2015-04-30 05:05:06 +0900

    Add StaticPagesController spec.

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  specify "should get home" do
+    get :home
+    expect(response).to have_http_status(:success)
+    # Title is tested in feature spec
+  end
+
+  specify "should get help" do
+    get :help
+    expect(response).to have_http_status(:success)
+  end
+
+  specify "should get about" do
+    get :about
+    expect(response).to have_http_status(:success)
+  end
+
+  specify "should get contact" do
+    get :contact
+    expect(response).to have_http_status(:success)
+  end
+end
