6056a9a9baf4a68d20b96a4cf97947845ee35918
Junichi Ito <jit@sonicgarden.jp>
2015-04-30 08:02:44 +0900

    Add UsersProfile spec.

---
+++ spec/features/users_profile_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "UsersProfile", type: :feature do
+  include ApplicationHelper
+
+  before do
+    @user = FactoryGirl.create :michael_with_microposts
+  end
+
+  specify "profile display" do
+    visit user_path(@user)
+    expect(page).to have_selector 'h1', text: @user.name
+    expect(page).to have_title full_title(@user.name)
+    expect(page).to have_selector 'h1>img.gravatar'
+    expect(page).to have_content @user.microposts.count
+    expect(page).to have_selector 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      expect(page).to have_content micropost.content
+    end
+  end
+end
