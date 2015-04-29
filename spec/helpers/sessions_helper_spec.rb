b6c6a707f14654a535ad742b09048f980ecc278c
Junichi Ito <jit@sonicgarden.jp>
2015-04-30 08:57:41 +0900

    Add SessionsHelper spec.

---
+++ spec/helpers/sessions_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe SessionsHelper, type: :helper do
+  before do
+    @user = FactoryGirl.create :michael
+    remember(@user)
+  end
+
+  specify "current_user returns right user when session is nil" do
+    expect(current_user).to eq @user
+    expect(is_logged_in?).to be_truthy
+  end
+
+  specify "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    expect(current_user).to be_nil
+  end
+end
