494ed99a4e986c73d28798135bf54c932fddf61b
Junichi Ito <jit@sonicgarden.jp>
2015-04-29 21:26:13 +0900

    Add micropost specs.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe Micropost, type: :model do
   specify "should be valid" do
     expect(@micropost.valid?).to be_truthy
   end
+  
+  specify "user id should be present" do
+    @micropost.user_id = nil
+    expect(@micropost.valid?).to be_falsey
+  end
+
+  specify "content should be present" do
+    @micropost.content = "   "
+    expect(@micropost.valid?).to be_falsey
+  end
+
+  specify "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    expect(@micropost.valid?).to be_falsey
+  end
+
+  # TODO Convert later
+  # test "order should be most recent first" do
+  #   assert_equal Micropost.first, microposts(:most_recent)
+  # end
 end
