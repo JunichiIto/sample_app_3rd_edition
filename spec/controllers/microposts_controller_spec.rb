f9de753562a55f606f8061bf29d23cd8dbfb8f7b
Junichi Ito <jit@sonicgarden.jp>
2015-04-30 05:26:09 +0900

    Add MicropostsController spec.

---
+++ spec/controllers/microposts_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe MicropostsController, type: :controller do
+  before do 
+    @micropost = FactoryGirl.create :orange
+  end
+
+  specify "should redirect create when not logged in" do
+    expect { post :create, micropost: { content: "Lorem ipsum" } }.to_not change { Micropost.count }
+    expect(response).to redirect_to login_url
+  end
+
+  specify "should redirect destroy when not logged in" do
+    expect { delete :destroy, id: @micropost }.to_not change { Micropost.count }
+    expect(response).to redirect_to login_url
+  end
+
+  specify "should redirect destroy for wrong micropost" do
+    user = @micropost.user # returns michael
+    log_in_as(user)
+    ants = FactoryGirl.create :ants
+    expect { delete :destroy, id: ants }.to_not change { Micropost.count }
+  end
+end
