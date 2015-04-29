260296f8542c22281e2c2ba698a7f14bd6bfe6c8
Junichi Ito <jit@sonicgarden.jp>
2015-04-30 05:15:37 +0900

    Add UsersController spec. / Refactoring other specs.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   specify "should get new" do
     get :new
-    expect(response).to have_http_status(:success)
+    expect(response).to have_http_status :success
   end
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  before do
+    @user = FactoryGirl.create :michael
+    @other_user = FactoryGirl.create :archer
+  end
+
+  specify "should redirect index when not logged in" do
+    get :index
+    expect(response).to redirect_to login_url
+  end
+
+  specify "should get new" do
+    get :new
+    expect(response).to have_http_status :success
+  end
+
+  specify "should redirect edit when not logged in" do
+    get :edit, id: @user
+    expect(flash.empty?).to be_falsey
+    expect(response).to redirect_to login_url
+  end
+
+  specify "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    expect(flash.empty?).to be_falsey
+    expect(response).to redirect_to login_url
+  end
+
+  xspecify "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    expect(flash.empty?).to be_truthy
+    expect(response).to redirect_to root_url
+  end
+
+  xspecify "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    expect(flash.empty?).to be_truthy
+    expect(response).to redirect_to root_url
+  end
+
+  specify "should redirect destroy when not logged in" do
+    expect { delete :destroy, id: @user }.to_not change { User.count }
+    expect(response).to redirect_to login_url
+  end
+
+  xspecify "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    expect { delete :destroy, id: @user }.to_not change { User.count }
+    expect(response).to redirect_to root_url
+  end
+
+  specify "should redirect following when not logged in" do
+    get :following, id: @user
+    expect(response).to redirect_to login_url
+  end
+
+  specify "should redirect followers when not logged in" do
+    get :followers, id: @user
+    expect(response).to redirect_to login_url
+  end
+end
