7bceb27fe23a638e1ab5e463396693532e0e08d2
Junichi Ito <jit@sonicgarden.jp>
2015-04-30 07:29:14 +0900

    Simulate a user clicking logout in a second window.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe SessionsController, type: :controller do
     get :new
     expect(response).to have_http_status :success
   end
+
+  specify "login with valid information followed by logout" do
+    user = FactoryGirl.create :michael
+    post :create, session: { email: user.email, password: 'password' }
+    expect(is_logged_in?).to be_truthy
+
+    delete :destroy
+    expect(is_logged_in?).to be_falsey
+
+    # Simulate a user clicking logout in a second window.
+    delete :destroy
+    expect(response).to redirect_to root_path
+  end
 end
