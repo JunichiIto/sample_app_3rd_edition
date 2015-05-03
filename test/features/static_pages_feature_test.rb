5a1314153bfab58b95844d5cba2bff63fcf484a1
Junichi Ito <jit@sonicgarden.jp>
2015-05-03 09:44:09 +0900

    Complete static pages feature test.

--- test/features/static_pages_feature_test.rb
+++ test/features/static_pages_feature_test.rb
@@ -5,4 +5,19 @@ class StaticPagesFeatureTest < Capybara::Rails::TestCase
     visit root_path
     assert_title "Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get help" do
+    visit help_path
+    assert_title "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    visit about_path
+    assert_title "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    visit contact_path
+    assert_title "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
