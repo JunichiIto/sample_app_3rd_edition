dc5c7f3f7f679c1fdb1e7f6fd266cde8fe34c6c8
Junichi Ito <jit@sonicgarden.jp>
2015-05-03 13:48:26 +0900

    Refactoring ajax test.

--- test/features/following_feature_test.rb
+++ test/features/following_feature_test.rb
@@ -7,36 +7,62 @@ class FollowingFeatureTest < Capybara::Rails::TestCase
     log_in_as(@user)
   end
 
-  test "following page" do
-    visit following_user_path(@user)
-    assert_not @user.following.empty?
-    assert_text @user.following.count
-    @user.following.each do |user|
-      assert has_link? nil, href: user_path(user)
+  class FollowingFeatureWithoutAjaxTest < FollowingFeatureTest
+    test "following page" do
+      visit following_user_path(@user)
+      assert_not @user.following.empty?
+      assert_text @user.following.count
+      @user.following.each do |user|
+        assert has_link? nil, href: user_path(user)
+      end
+    end
+
+    test "followers page" do
+      visit followers_user_path(@user)
+      assert_not @user.followers.empty?
+      assert_text @user.followers.count
+      @user.followers.each do |user|
+        assert has_link? nil, href: user_path(user)
+      end
+    end
+
+    test "should follow a user the standard way" do
+      visit user_path(@other)
+      assert_difference '@user.following.count', 1 do
+        click_button 'Follow'
+      end
+    end
+
+    test "should unfollow a user the standard way" do
+      @user.follow(@other)
+      visit user_path(@other)
+      assert_difference '@user.following.count', -1 do
+        click_button 'Unfollow'
+      end
     end
   end
 
-  test "followers page" do
-    visit followers_user_path(@user)
-    assert_not @user.followers.empty?
-    assert_text @user.followers.count
-    @user.followers.each do |user|
-      assert has_link? nil, href: user_path(user)
+  class FollowingFeatureWithAjaxTest < FollowingFeatureTest
+    def setup
+      Capybara.current_driver = Capybara.javascript_driver
+      super
     end
-  end
 
-  test "should follow a user the standard way" do
-    visit user_path(@other)
-    assert_difference '@user.following.count', 1 do
-      click_button 'Follow'
+    test "should follow a user with Ajax" do
+      visit user_path(@other)
+      assert_difference '@user.following.count', 1 do
+        click_button 'Follow'
+        sleep 0.2
+      end
     end
-  end
 
-  test "should unfollow a user the standard way" do
-    @user.follow(@other)
-    visit user_path(@other)
-    assert_difference '@user.following.count', -1 do
-      click_button 'Unfollow'
+    test "should unfollow a user with Ajax" do
+      @user.follow(@other)
+      visit user_path(@other)
+      assert_difference '@user.following.count', -1 do
+        click_button 'Unfollow'
+        sleep 0.2
+      end
     end
   end
 end
